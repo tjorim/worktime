486f167037ae200c05b94ad3f78a8900623d0e40
Jorim Tielemans <tielemans.jorim@gmail.com>
2026-02-09 22:23:16 +0100

    Refactor header to use span instead of h6

--- src/components/schedule/TodayView.tsx
+++ src/components/schedule/TodayView.tsx
@@ -214,13 +214,13 @@ export function TodayView({
     <Card>
       <Card.Header>
         <div className="d-flex justify-content-between align-items-center mb-2">
-          <h6 className="mb-0">
+          <span className="fw-semibold">
             <i
               className={`bi ${hasTeams ? "bi-people" : "bi-calendar2"} me-2`}
               aria-hidden="true"
             ></i>
             {hasTeams ? "All Teams" : "Schedule"}
-          </h6>
+          </span>
           <DayNavigationButtonGroup
             isCurrent={isToday}
             onPrevious={onPreviousDay}
